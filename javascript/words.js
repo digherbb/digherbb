9441c2d22ca2fce2bb04321cb4577defe59bf15d
Digherb Boucher <126331436+digherbb@users.noreply.github.com>
2023-10-24 23:19:36 -0400

    Add files via upload

---
+++ javascript/words.js
@@ -0,0 +1,77 @@
+const wordList = [
+    {
+        word: "david",
+        hint: "Best correlation one TA"
+    },
+    {
+        word: "barbie",
+        hint: "Movie About a Plastic Doll"
+    },
+    {
+        word: "scream",
+        hint: "Ghostface"
+    },
+    {
+        word: "candyman",
+        hint: "Say His Name 3 Times if You Dare"
+    },
+    {
+        word: "oppenheimer",
+        hint: "Father of The Atomic Bomb"
+    },
+    {
+        word: "seven",
+        hint: "What's in The Box?"
+    },
+    {
+        word: "grinch",
+        hint: "He's a Mean One"
+    },
+    {
+        word: "speed",
+        hint: "How Do I Stop this Bus"
+    },
+    {
+        word: "halloween",
+        hint: "Jamie Lee Curtis Can't Catch a Break"
+    },
+    {
+        word: "jaws",
+        hint: "You're gonna need a bigger boat"
+    },
+    {
+        word: "shrek",
+        hint: "Do The Roar!"
+    },
+    {
+        word: "interstallar",
+        hint: "Hans Zimmer + Christopher Nolan"
+    },
+    {
+        word: "beetlejuice",
+        hint: "Couple Drowned Then Came Back"
+    },
+    {
+        word: "superbad",
+        hint: "McLovin'"
+    },
+    {
+        word: "avatar",
+        hint: "Why Did This Movie make 2.923 billion USD?"
+    },
+    {
+        word: "scarface",
+        hint: "The eyes, chico. They never lie."
+    },
+    {
+        word: "oldboy",
+        hint: "Vengence Gone Too Far"
+    },
+    {
+        word: "klaus",
+        hint: "best animated christmas movie"
+    },
+
+
+]
+    
